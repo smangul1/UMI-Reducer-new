f35ea86e8c36464d39c1f45eff0f041475d88420
garrett-parker <garrett.parker@me.com>
2017-10-27 13:46:44 -0700

    Update collapsePCRduplicates.py

--- collapsePCRduplicates.py
+++ collapsePCRduplicates.py
@@ -29,26 +29,26 @@ ap.add_argument('outbam', help='Output file to save reads after collapsing PCR d
 #                'print out some example feature IDs and their attributes')
 ap.add_argument('--m', action='store_true',help='Save multi-mapped reads')
 ap.add_argument('--c', action='store_true',help='Change chromosome format')
- 
+
 #cmd https://gist.github.com/daler/ec481811a44b3aa469f3
- 
+
 args = ap.parse_args()
- 
- 
- 
- 
- 
+
+
+
+
+
 bam=args.inbam
 out=args.outbam
 
- 
- 
+
+
 chr_list=[]
 
 if args.c:
     for i in range(1,20):
         chr_list.append('chr'+str(i))
- 
+
     chr_list.append('chrX')
     chr_list.append('chrY')
     chr_list.append('chrM')
@@ -56,65 +56,70 @@ if args.c:
 else:
     for i in range(1,20):
         chr_list.append(str(i))
- 
+
     chr_list.append('X')
     chr_list.append('Y')
     chr_list.append('MT')
- 
-print chr
- 
+
+#print chr
+
 position=[]
 position_all_uniq=[]
- 
- 
- 
- 
- 
+
+
+
+
+
 samfile = pysam.AlignmentFile(bam, "rb" )
- 
- 
+
+
 dict= {}
- 
- 
+
+
 mappedReads=[]
 numberReadsUnique=0
- 
+
 numberReadsUniquePlusMultiMapped=0
- 
+
 numberReadsUnique_covGreated1=0
 numberReadsUnique_filtered=0
 readLength=[]
 readLength_filtered=[]
 before=0
 after=0
- 
- 
- 
- 
+
+
+
+
 readSet=set()
- 
- 
+
+
 bam_header = pysam.Samfile(bam, 'rb').header
- 
- 
- 
+
+
+
 outfile = pysam.AlignmentFile(out, "wb", header=bam_header)
- 
- 
- 
+
+
+
 print "Open ",bam, "via pysam"
- 
- 
- 
- 
+
+
+
+
 for chr in chr_list:
     dict.clear()
     position[:]=[]
-    print "----------chr",chr
+
+    if args.c:
+        print "----------",chr
+    else:
+        print "----------chr",chr
     for read in samfile.fetch(chr):
+        #stores the name of the read in mappedReads
         mappedReads.append(read.query_name)
-         
-         
+
+
         if args.m:
             if read.mapq==50:
                 numberReadsUnique+=1
@@ -122,31 +127,35 @@ for chr in chr_list:
             position.append(read.reference_start)
             readLength.append(len(read.query_sequence))
         else:
+            #checks that the mapping quality of the read is equal to 50
             if read.mapq==50:
+                #increments numberReadsUnique
                 numberReadsUnique+=1
+                #stores the start position of the read
                 position.append(read.reference_start)
+                #stores the length of the read
                 readLength.append(len(read.query_sequence))
- 
- 
-    print "numberReadsUnique",numberReadsUnique
-    print "numberReadsUniquePlusMultiMapped",numberReadsUniquePlusMultiMapped
- 
- 
- 
+
+
+    print "numberReadsUnique: ",numberReadsUnique
+    print "numberReadsUniquePlusMultiMapped: ",numberReadsUniquePlusMultiMapped
+
+
+    #creates dictionary consisting of read position values
     counter_chr=collections.Counter(position)
     position_all_uniq+=position
-    print "Number of position with #reads starting >=1", len(position)
- 
+    print "Number of read positions stored: ", len(position)
+
     count=0
- 
+
     print  "Processing", len(counter_chr.items()), "items"
- 
+
     for key,val in counter_chr.items():
         #print key,val
         if count%10000==1:
             print count
         count+=1
-         
+
         if val==1:
             for read in samfile.fetch(chr,key,key+1):
                 if read.reference_start==key:
@@ -154,16 +163,16 @@ for chr in chr_list:
                     readLength_filtered.append(len(read.query_sequence))
                     numberReadsUnique_filtered+=1
                     readSet.add(read.query_name)
-      
-     
- 
- 
- 
- 
+
+
+
+
+
+
         elif val>1:
             setReads=set()
             setReads.clear()
-     
+
             if val>1000:
                 print val, chr,key
             Read=[]
@@ -172,13 +181,13 @@ for chr in chr_list:
                 if read.reference_start==key:
                     Read.append(read)
                     setReads.add(read.query_name.split("_")[3]+"_"+read.query_sequence)
- 
+
         #print key,val
- 
- 
-             
-             
- 
+
+
+
+
+
             notsetReads=set()
             notsetReads.clear()
             numberReadsUnique_covGreated1+=len(setReads)
@@ -189,84 +198,84 @@ for chr in chr_list:
                         readLength_filtered.append(len(Read[i].query_sequence))
                         notsetReads.add(Read[i].query_name.split("_")[3]+"_"+Read[i].query_sequence)
                         readSet.add(Read[i].query_name)
- 
- 
- 
-                     
- 
+
+
+
+
+
 outfile.close()
- 
- 
- 
+
+
+
 #-----------------------
 #statistics
- 
- 
+
+
 header=[]
- 
+
 header.append('sample')
 header.append('Number of mapped reads')
 header.append('Number of reads mapped to unique location (UNIQUE reads)')
 header.append('Number of reads alligments after collapsing PCR dublicated (an aligment may include several copies of reads) ')
 header.append('Number of reads after collapsing PCR dublicated (each read is present once) ')
- 
- 
+
+
 nr=[]
- 
+
 nr.append(out.split('.')[0])
 nr.append(len(set(mappedReads)))
 nr.append(numberReadsUnique)
- 
- 
+
+
 nr.append(numberReadsUnique_filtered)
 nr.append(len(readSet))
- 
- 
- 
- 
+
+
+
+
 stat_f=out.split('.')[0]+'.number_of_reads_stat'
- 
+
 with open(stat_f, 'w') as fp:
     a = csv.writer(fp, delimiter=',')
     a.writerow(header)
     a.writerow(nr)
- 
- 
- 
- 
+
+
+
+
 counter=collections.Counter(position_all_uniq)
 position_all_uniq=set(position_all_uniq)
 print "Number of position with #reads staring >=1", len(position_all_uniq)
- 
- 
- 
- 
+
+
+
+
 #-----------------------
 #save as a histogram - counter_length_filtered
- 
+
 x1=[]
 xbins1=[]
- 
+
 counter_length_filtered=collections.Counter(readLength_filtered)
 for key,val in counter_length_filtered.items():
     #print key,val
     xbins1.append(val)
     x1.append(key)
- 
- 
- 
+
+
+
 plot1=out.split('.')[0]+'.readLengthPCRDuplicatesCollapsed.png'
 print "save to",plot1
- 
- 
- 
+
+
+
 plt.title('Length of reads after collapsing PCR duplicates')
- 
+
 plt.bar(x1,xbins1)
 plt.savefig(plot1)
- 
- 
- 
+
+
+
 #save as a histogram - counter_length_uniq
 x2=[]
 xbins2=[]
@@ -276,18 +285,18 @@ for key,val in counter_length.items():
     #print key,val
     xbins2.append(val)
     x2.append(key)
- 
+
 plot2=out.split('.')[0]+'.readLengthBeforePCRduplicates.png'
 print "save to",plot2
- 
- 
- 
+
+
+
 plt.title('Length before collapsing PCR duplicates')
 plt.bar(x2,xbins2)
 plt.savefig(plot2)
- 
- 
- 
- 
- 
+
+
+
+
+
 print "DONE!"
